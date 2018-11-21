9debaddf8fc99d597a886484d8fa6325c4addb61
slen99 <1558615303@qq.com>
2018-11-22 00:39:05 +0800

    profile实现修改密码

--- login/forms.py
+++ login/forms.py
@@ -3,10 +3,22 @@ from django.contrib.auth import authenticate
 from .models import ManagerUser, RegisterCode
 
 
+def checkio(s):
+    fs = ''.join(filter(str.isalnum, s)) # keep only letters and digits
+    return (
+            len(fs) >= 1        # There is at least one letter or digit
+        and len(s)  >= 8       # ... and there are at least 10 characters
+        and not fs.isalpha()    # ... and there is at least one digit
+        and not fs.isdigit()    # ... and there is at least one letter
+        and not fs.islower()    # ... and not all letters are lowercase
+        and not fs.isupper()    # ... and not all letters are uppercase
+    )
+
 class LoginForm(forms.Form):
     username = forms.CharField(label='用户名',
                              widget=forms.TextInput(attrs={
                                  'class':'form-control',
+                                 'id': "disableinput",
                                  'placeholder':'请输入用户名',
                              }))
     password = forms.CharField(label='密码', widget=forms.PasswordInput(attrs={
@@ -24,3 +36,43 @@ class LoginForm(forms.Form):
         else:
             self.cleaned_data['user'] = user
         return self.cleaned_data
+
+
+class ProfileForm(forms.Form):
+    old_password = forms.CharField(label='旧密码', widget=forms.PasswordInput(attrs={
+                                 'class': 'form-control',
+                                 'placeholder': '请输入旧的密码',
+                             }))
+    new_password = forms.CharField(label='新密码', widget=forms.PasswordInput(attrs={
+                                 'class': 'form-control',
+                                 'placeholder': '请输入新的密码',
+                             }))
+    new_password_re = forms.CharField(label='确认新密码', widget=forms.PasswordInput(attrs={
+                                 'class': 'form-control',
+                                 'placeholder': '请确认新的密码',
+                             }))
+
+    def __init__(self, *args, **kwargs):
+        if 'user' in kwargs:
+            self.user = kwargs.pop('user')
+        super(ProfileForm, self).__init__(*args, **kwargs)
+
+    def clean_old_password(self):
+        old_pass = self.cleaned_data.get('old_password','')
+        if not self.user.check_password(old_pass):
+            raise forms.ValidationError('旧的密码错误')
+        return old_pass
+
+    def clean_new_password(self):
+        new_password = self.cleaned_data.get('new_password', '')
+        if not checkio(new_password):
+            raise forms.ValidationError("密码至少8位并包含一个字母")
+        return new_password
+
+    def clean_new_password_re(self):
+        new_password = self.cleaned_data.get('new_password', '')
+        new_password_re = self.cleaned_data.get('new_password_re', '')
+        if new_password != new_password_re or new_password == '':
+            raise forms.ValidationError("两次输入密码不一致")
+        return new_password_re
+
--- login/views.py
+++ login/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import login, logout
-from .forms import LoginForm
+from .forms import LoginForm, ProfileForm
 from .models import ManagerUser
 
 # Create your views here.
@@ -26,7 +26,7 @@ def home_page(request):
     if request.user.is_authenticated:
         return render(request, "index.html")
     else:
-        return redirect('login/')
+        return redirect('login')
 
 def user_logout(request):
     logout(request)
@@ -35,5 +35,21 @@ def user_logout(request):
 def forget_password(request):
     return render(request, "forgot.html")
 
-def user_profile(requset):
-    return render(requset, "profiles.html")
+def user_profile(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    else:
+        if request.method == 'POST':
+            Profile_form = ProfileForm(request.POST, user=request.user)
+            if Profile_form.is_valid():
+                user = request.user
+                new_pass = Profile_form.cleaned_data['new_password']
+                user.set_password(new_pass)
+                user.save()
+                logout(request)
+                return redirect('login')
+        else:
+            Profile_form = ProfileForm()
+
+        context = {'Profile_form': Profile_form, }
+        return render(request, "profiles.html", context)
