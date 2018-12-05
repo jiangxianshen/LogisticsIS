36f1f8d8cd938c5938aadc6963ad6b0aa30948c6
slen99 <1558615303@qq.com>
2018-12-06 01:03:02 +0800

    notification0.9

--- notification/urls.py
+++ notification/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.notification, name="notification"),
+    path('list/<int:page_num>', views.notification, name="notification"),
     path('detail/<int:article_id>', views.detail, name='detail'),
 ]
--- notification/views.py
+++ notification/views.py
@@ -6,13 +6,15 @@ from .models import Notification
 
 def detail(request, article_id):
     blogs = get_object_or_404(Notification, id=article_id)
-    content = {"blogs": blogs}
+    infer = request.GET.get('infer')
+    content = {"blogs": blogs, 'infer':infer}
     return render(request, "notification_detail.html", content)
 
 
-def notification(request):
-    articles = Notification.objects.filter(recommend=False)
+def notification(request, page_num):
+    page = Paginator(Notification.objects.filter(recommend=False), per_page=10)
+    articles = page.page(page_num)
     top_articles = Notification.objects.filter(recommend=True)
-    content = {"articles": articles, "top_articles":top_articles}
+    content = {"articles": articles, "top_articles":top_articles, 'page_num':page_num, 'count':page.count//10+1}
     return render(request, "notification.html", content)
 
